1b03dfb6f95a667806c2b17781bd4c77e7f6d5f8
MahbubHosssen <mhbabu2002@gmail.com>
2025-01-18 19:31:56 +0600

    install react router, tailwind shadcn

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import { Button } from "@/components/ui/button"
+
+function App() {
+
+  return (
+    <>
+      <button className="text-red-400">btn</button>
+      <Button>btn</Button>
+    </>
+  )
+}
+
+export default App
